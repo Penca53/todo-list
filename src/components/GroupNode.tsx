e5bd0c49fdb880b4809c918057ac8a3014080243
Radiaxx <daniscrima.2003@gmail.com>
2022-11-13 00:30:59 +0100

    Fixed groups css

--- src/components/GroupNode.tsx
+++ src/components/GroupNode.tsx
@@ -19,6 +19,7 @@ const GroupNode: React.FC<GroupNodeProps> = (props) => {
   const createTodoGroup = trpc.todoGroup.createTodoGroups.useMutation();
 
   const handleAddGroupClick = () => {
+    console.log(props.groupNode.item);
     createTodoGroup
       .mutateAsync({
         name: addGroup.name,
@@ -28,81 +29,88 @@ const GroupNode: React.FC<GroupNodeProps> = (props) => {
   };
 
   return (
-    <div className="ml-6 flex-row bg-gray-500">
-      <button
-        onClick={() => props.onGroupClick(props.groupNode.item)}
-        className="btn btn-ghost justify-start"
-      >
-        {props.groupNode.item ? props.groupNode.item.name : session?.user.name}
-      </button>
-
-      <label
-        htmlFor="create-new-group-modal"
-        className="modal-button btn btn-ghost justify-end"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="h-6 w-6"
+    <div className="ml-6">
+      <div className="flex justify-between  ">
+        <button
+          onClick={() => props.onGroupClick(props.groupNode.item)}
+          className="btn btn-ghost"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
-      </label>
+          {props.groupNode.item
+            ? props.groupNode.item.name
+            : session?.user.name}
+        </button>
 
-      <input
-        type="checkbox"
-        id="create-new-group-modal"
-        className="modal-toggle"
-      />
-
-      <div className="modal">
-        <div className="modal-box relative">
-          <label
-            htmlFor="create-new-group-modal"
-            className="btn btn-circle btn-sm absolute right-2 top-2"
+        <label
+          htmlFor="create-new-group-modal"
+          className="modal-button btn btn-ghost"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6"
           >
-            ✕
-          </label>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </label>
 
-          <div>
-            <form className="mb-4 rounded px-8 pt-3">
-              <div className="mb-4">
-                <label className="mb-2 block text-sm font-bold" htmlFor="name">
-                  New Group Name
-                </label>
+        <input
+          type="checkbox"
+          id="create-new-group-modal"
+          className="modal-toggle"
+        />
 
-                <input
-                  className="input input-bordered w-full max-w-xs"
-                  id="name"
-                  type="text"
-                  placeholder="Name..."
-                  value={addGroup.name}
-                  onChange={(e) => {
-                    setAddGroup((prevState) => ({
-                      ...prevState,
-                      name: e.target.value,
-                    }));
-                  }}
-                ></input>
+        <div className="modal">
+          <div className="modal-box relative">
+            <label
+              htmlFor="create-new-group-modal"
+              className="btn btn-circle btn-sm absolute right-2 top-2"
+            >
+              ✕
+            </label>
 
-                <div className="mt-6 flex justify-start">
-                  <button
-                    className="btn rounded"
-                    type="button"
-                    onClick={handleAddGroupClick}
+            <div>
+              <form className="mb-4 rounded px-8 pt-3">
+                <div className="mb-4">
+                  <label
+                    className="mb-2 block text-sm font-bold"
+                    htmlFor="name"
                   >
-                    Add Group
-                  </button>
+                    New Group Name
+                  </label>
+
+                  <input
+                    className="input input-bordered w-full max-w-xs"
+                    id="name"
+                    type="text"
+                    placeholder="Name..."
+                    value={addGroup.name}
+                    onChange={(e) => {
+                      setAddGroup((prevState) => ({
+                        ...prevState,
+                        name: e.target.value,
+                      }));
+                    }}
+                  ></input>
+
+                  <div className="mt-6 flex justify-start">
+                    <button
+                      className="btn rounded"
+                      type="button"
+                      onClick={handleAddGroupClick}
+                    >
+                      Add Group
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
       </div>
