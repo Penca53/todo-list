f594b76ae7b4075e61be87a9c1ab87b908c9707d
Penca53 <pencapencator@gmail.com>
2022-11-12 23:40:25 +0100

    Adjust default status.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
   const [addTodoDescription, setAddTodoDescription] = useState<string | null>(
     null
   );
-  const [addTodoStatus, setAddTodoStatus] = useState<boolean | null>(null);
+  const [addTodoStatus, setAddTodoStatus] = useState<boolean>(false);
 
   const [selectedTodoGroup, setSelectedTodoGroup] = useState<TodoGroup | null>(
     null
@@ -63,7 +63,7 @@ const Home: NextPage = () => {
   const handleAddTodoClick = () => {
     setIsAddingTodo(true);
 
-    if (!addTodoName || !addTodoDescription || !addTodoStatus) {
+    if (!addTodoName || !addTodoDescription) {
       setIsAddingTodo(false);
       return;
     }
@@ -121,26 +121,6 @@ const Home: NextPage = () => {
   return (
     <Layout>
       <div className="flex h-full">
-        {/*
-          <ul className="mt-8">
-          {getTodos.isLoading
-            ? "Loading..."
-            : getTodos.isError
-            ? "Error!"
-            : getTodos.data
-            ? getTodos.data.map((todo) => (
-                <TodoItemComponent
-                  key={todo.id}
-                  todoItem={todo}
-                  onTodoItemChangeIsFavourite={handleTodoItemChangeIsFavourite}
-                  onTodoItemChangeStatus={handleTodoItemChangeStatus}
-                  onTodoItemDelete={handleTodoItemDelete}
-                />
-              ))
-            : null}
-        </ul>
-          */}
-
         <div className="w-96 overflow-y-scroll border-r border-gray-500 p-4 shadow shadow-gray-400">
           <ul className="mt-4">
             {getTodoGroups.isLoading ? (
@@ -203,7 +183,7 @@ const Home: NextPage = () => {
               setIsAddTodoModalOpen(e.target.checked);
               setAddTodoName(null);
               setAddTodoDescription(null);
-              setAddTodoStatus(null);
+              setAddTodoStatus(false);
             }}
           />
           <label htmlFor="create-new-todo-modal" className="modal">
@@ -292,96 +272,6 @@ const Home: NextPage = () => {
             <div>Hello</div>
           </div>
         </div>
-
-        {/*
-            <ul className="mt-4">
-          {getTodoGroups.isLoading ? (
-            "Loading..."
-          ) : getTodoGroups.isError ? (
-            "Error!"
-          ) : getTodoGroups.data ? (
-            <GroupNode
-              todos={getTodos.data!}
-              groupNode={createGroupTree(getTodoGroups.data)}
-              height={0}
-            />
-          ) : null}
-        </ul>
-        <div className="mt-8">
-          <form className="mb-4 rounded bg-white px-8 pt-6 pb-8 shadow-md">
-            <div className="mb-4">
-              <label
-                className="mb-2 block text-sm font-bold text-gray-700"
-                htmlFor="name"
-              >
-                Name
-              </label>
-              <input
-                className="focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
-                id="name"
-                type="text"
-                placeholder="Name..."
-                value={addTodo.name}
-                onChange={(e) => {
-                  setAddTodo((prevState) => ({
-                    ...prevState,
-                    name: e.target.value,
-                  }));
-                }}
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                className="mb-2 block text-sm font-bold text-gray-700"
-                htmlFor="description"
-              >
-                Description
-              </label>
-              <textarea
-                className="focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
-                id="description"
-                placeholder="Description..."
-                value={addTodo.description}
-                onChange={(e) => {
-                  setAddTodo((prevState) => ({
-                    ...prevState,
-                    description: e.target.value,
-                  }));
-                }}
-              />
-            </div>
-            <div className="mb-6">
-              <label
-                className="mb-2 block text-sm font-bold text-gray-700"
-                htmlFor="status"
-              >
-                Status
-              </label>
-              <input
-                className="h-6 w-6 rounded border-gray-300 bg-gray-100 text-blue-600 shadow focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
-                id="status"
-                type="checkbox"
-                checked={addTodo.status}
-                onChange={() => {
-                  setAddTodo((prevState) => ({
-                    ...prevState,
-                    status: !prevState.status,
-                  }));
-                }}
-              />
-            </div>
-            <div className="flex justify-center">
-              <button
-                className="focus:shadow-outline rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700 focus:outline-none"
-                type="button"
-                onClick={handleAddTodoClick}
-              >
-                Add Todo
-              </button>
-            </div>
-          </form>
-        </div>
-            */}
       </div>
     </Layout>
   );
